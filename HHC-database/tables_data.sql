ce6e344817d7af74119aea8e9bfa77cacec26eb5
Muse Eskinder <museeskider@users.noreply.github.com>
2023-06-20 14:03:12 +0300

    tables data

---
+++ HHC-database/tables_data.sql
@@ -0,0 +1,17 @@
+insert into employee(emp_id, first_name, last_name, profession, phone_no, sex) values(01, 'Abel', 'Mesfin', 'Manager', '251911893434', 'M');
+insert into employee(emp_id, first_name, last_name, profession, phone_no, sex) values(01, 'Samrawit', 'Wubet', 'Assistant', '25191234343', 'F');
+insert into employee(emp_id, first_name, last_name, profession, phone_no, sex) values(01, 'Melsew', 'Adise', 'Forman', '251911334455', 'M');
+insert into employee(emp_id, first_name, last_name, profession, phone_no, sex) values(01, 'Gush', 'Teklay', 'Driver', '25191100000', 'M');
+insert into employee(emp_id, first_name, last_name, profession, phone_no, sex) values(01, 'Hailay', 'Kebede', 'Driver', '25191900323', 'M');
+insert into employee(emp_id, first_name, last_name, profession, phone_no, sex) values(01, 'Teshome', 'Zeleke', 'Civil Enginner', '251911800234', 'M');
+insert into employee(emp_id, first_name, last_name, profession, phone_no, sex) values(01, 'Abel', 'Kifle', 'Survey', '25191184343', 'M');
+insert into employee(emp_id, first_name, last_name, profession, phone_no, sex) values(01, 'Muse', 'Eskinder', 'Data Analyst', '251975343477', 'M');
+
+#capital_table
+
+insert into capital(name, owned_date, capital_identification, purpose, project_name) values('sino-95029', '8-20-2015', 'HHC-001', 'construction truck', 'airport_parking');
+
+#project_table
+
+insert into project(name, start_date, deadline_date, type, address) values('airport_parking', '2-12-2021', '8-10-2024', 'structural', 'bole airport');
+insert into project(name, start_date, deadline_date, type, address) values('HHC-ASER', '4-23-2021', '5-26-2024', 'supply', 'varies');
